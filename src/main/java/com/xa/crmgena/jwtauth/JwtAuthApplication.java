ce9359dbdf0d1967f932a52b414c76950c6421ad
gagishmagi <gagi.shmagi@gmail.com>
2024-01-06 15:44:56 +0200

    Added auto add to admin/moderator/user roles for backend if not exist

--- src/main/java/com/xa/crmgena/jwtauth/JwtAuthApplication.java
+++ src/main/java/com/xa/crmgena/jwtauth/JwtAuthApplication.java
@@ -1,7 +1,13 @@
 package com.xa.crmgena.jwtauth;
 
+import com.xa.crmgena.jwtauth.models.Role;
+import com.xa.crmgena.jwtauth.repositories.RoleRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JwtAuthApplication {
@@ -10,4 +16,20 @@ public class JwtAuthApplication {
         SpringApplication.run(JwtAuthApplication.class, args);
     }
 
+
+    private static final Logger log =  LoggerFactory.getLogger(JwtAuthApplication.class);
+
+    @Bean
+    CommandLineRunner initDatabase(RoleRepository roleRepository){
+        return args -> {
+            if(roleRepository.count() == 0){
+                log.info("Preloading " + roleRepository.save(
+                        new Role("ROLE_ADMIN")));
+                log.info("Preloading " + roleRepository.save(
+                        new Role("ROLE_USER")));
+                log.info("Preloading " + roleRepository.save(
+                        new Role("ROLE_MODERATOR")));
+            }
+        };
+    }
 }
